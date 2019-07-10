476007ae1855abd0ba4f57b8a590bb8c257f64da
Lucas Ribeiro <lksribeiro2014@gmail.com>
2019-07-09 23:31:36 -0300

    connection mongoDB

--- models/ListaTransacoes.js
+++ models/ListaTransacoes.js
@@ -8,20 +8,22 @@ class ListaTransacoes{
   adiciona(transacao) {
     this._transacoes.unshift(transacao);
 
-    // chamando conexao
-    var MongoClient = require('./../mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/";
+    console.log(this._transacoes);
 
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("mydb");
-      var myobj = this._transacoes;
-      dbo.collection("customers").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    });
+    // chamando conexao
+    // var MongoClient = require('./../node_modules/mongodb').MongoClient;
+    // var url = "mongodb://localhost:27017/";
+
+    // MongoClient.connect(url, function(err, db) {
+    //   if (err) throw err;
+    //   var dbo = db.db("mydb");
+    //   var myobj = this._transacoes;
+    //   dbo.collection("customers").insertOne(myobj, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("1 document inserted");
+    //     db.close();
+    //   });
+    // });
   }
 
   get transacoes(){
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-// chamando conexao
-var MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/transacoesBD', {useNewUrlParser: true}, (err) => {
+  if (!err) { console.log('MongoDB Connection Succeded.')}
+});
 
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+
+  // define Schema
+  var TransacaoSchema = mongoose.Schema({
+    tipo: String,
+    nome: String,
+    valor: String
+  });
+
+  // compile schema to model
+  var transacoes = mongoose.model('Transacao', TransacaoSchema, 'listaTransacoes');
+
+  // a document instance
+  var transacao = new transacoes({ tipo: '-', nome: 'PC', valor: '15000' });
+
+  // save model to database
+  transacao.save(function (err, transacao) {
+    if (err) return console.error(err);
+    console.log(transacao.nome + " saved");
+  });
+
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,4 +40,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-module.exports = MongoClient;
+
